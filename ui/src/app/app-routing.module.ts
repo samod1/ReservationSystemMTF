cac71e07b526ca4c0acf0a8d591411a6eb90bac9
Samuel Domin <samuel.domin@dominanz.sk>
2022-09-23 09:14:42 +0200

    Building  - rooting  - vytvorenie noveho komponentu  - novy model a serivce

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { Routes, RouterModule } from "@angular/router";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { RezervacieComponent } from "./components/rezervacie/rezervacie.component";
 import { CreateReservationComponent } from "./components/rezervacie/create-reservation/create-reservation.component";
-
+import { BuildingComponent} from "./components/building/building.component";
 
 
 const routes: Routes = [
     {path: 'dashboard-component', component: DashboardComponent},
     {path: 'rezervacie-component', component: RezervacieComponent},
-    {path: 'create-reservation-component', component: CreateReservationComponent}
+    {path: 'create-reservation-component', component: CreateReservationComponent},
+    {path: 'building-component', component: BuildingComponent}
 ];
 @NgModule({
     imports:[RouterModule.forRoot(routes)],
