23afde195d0d76df9aee3b209522b26341be4127
Samuel Domin <samuel.domin@dominanz.sk>
2022-10-09 10:54:41 +0200

    vytvorenie cesty pre user component

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { RezervacieComponent } from "./components/rezervacie/rezervacie.component";
 import { CreateReservationComponent } from "./components/rezervacie/create-reservation/create-reservation.component";
 import { BuildingComponent} from "./components/building/building.component";
+import { UserComponent } from "./components/user/user.component";
 
 
 const routes: Routes = [
     {path: 'dashboard-component', component: DashboardComponent},
     {path: 'rezervacie-component', component: RezervacieComponent},
     {path: 'create-reservation-component', component: CreateReservationComponent},
-    {path: 'building-component', component: BuildingComponent}
+    {path: 'building-component', component: BuildingComponent},
+    {path: 'user-component', component: UserComponent}
 ];
 @NgModule({
     imports:[RouterModule.forRoot(routes)],
