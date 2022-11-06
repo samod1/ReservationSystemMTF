0eb2ab977747d347b7d8817a4ac467bea1293d32
Samuel Domin <samuel.domin@dominanz.sk>
2022-11-06 18:15:38 +0100

    pridanie DashboardControlleru

---
+++ rest/src/main/java/sk/mtf/is/rezervacnySystem/controller/DashboardController.java
@@ -0,0 +1,16 @@
+package sk.mtf.is.rezervacnySystem.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.util.Date;
+
+@RestController
+@CrossOrigin("localhost:4200")
+public class DashboardController {
+
+
+}
